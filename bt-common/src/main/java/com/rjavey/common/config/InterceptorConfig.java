018c63f6e76aecd6302b8b4871b4ad845a696f8d
rjavey <1099779409@qq.com>
2023-12-17 23:35:23 +0800

    login todo

--- bt-common/src/main/java/com/rjavey/common/config/InterceptorConfig.java
+++ bt-common/src/main/java/com/rjavey/common/config/InterceptorConfig.java
@@ -1,7 +1,9 @@
 package com.rjavey.common.config;
 
 import com.rjavey.common.interceptors.UserInfoInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @create: 2023-12-17 23:14:13
  **/
 @Configuration
+@ConditionalOnClass(DispatcherServlet.class)
 public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
